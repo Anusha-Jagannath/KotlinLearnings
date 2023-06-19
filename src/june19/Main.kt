0504e276645e0be12a71028e167993267feba3d4
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-06-19 14:38:28 +0530

    Added code for sealed classes

---
+++ src/june19/Main.kt
@@ -0,0 +1,24 @@
+package june19
+
+fun main(args: Array<String>) {
+    println("Learning kotlin basics")
+    //single line comment
+    /**
+     * this is multi line comment
+     */
+
+    var myString = "Hello world"
+    println(myString)
+
+    var myDecimal = 1.0
+    println(myDecimal)
+
+    var string: String
+    string = "hello world"
+    string = "another world"
+
+    val secondString: String
+    secondString = "myString"
+
+
+}
