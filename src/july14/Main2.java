6527abf4ccab085684d8f417f3b20c64d001f7a2
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-07-15 19:49:46 +0530

    Added cod for maps

---
+++ src/july14/Main2.java
@@ -0,0 +1,13 @@
+package july14;
+
+public class Main2 {
+
+    public static void main(String args[]) {
+        int sum = MainKt.add(2, 3);
+        System.out.println(sum);
+    }
+
+    public static int sumOf(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/july15/Main.kt
@@ -0,0 +1,11 @@
+package july15
+
+fun main(args: Array<String>) {
+    println("Learning interoperability")
+    println(Main2.getArea(2,3))
+
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b;
+}
