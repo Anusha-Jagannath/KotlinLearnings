fcde60b62d067026dff5854b496717acedbdf169
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-07-16 12:56:20 +0530

    Added code for hashaps

---
+++ src/july16/Main2.java
@@ -0,0 +1,9 @@
+package july16;
+
+public class Main2 {
+
+    public static void main(String args[]) {
+        int result = MainKt.findVolume(4, 7);
+        System.out.println("The result is " + result);
+    }
+}
